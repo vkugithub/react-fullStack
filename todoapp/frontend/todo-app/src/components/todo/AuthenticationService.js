b4da8b5960075418184515b16c8e5af53aed1d82
vkumar.kumar <vkumar112@sapient.com>
2019-11-10 10:17:48 +0530

    Authentication by JWT

--- todoapp/frontend/todo-app/src/components/todo/AuthenticationService.js
+++ todoapp/frontend/todo-app/src/components/todo/AuthenticationService.js
@@ -1,9 +1,39 @@
+import axios from 'axios'
+
 class AuthenticationService {
 
-    registerSuccessfulLogin(username,password){
-        console.log('registerSuccessfulLogin')
-        sessionStorage.setItem('authenticatedUser', username);
+    executeBasicAuthenticationService(username, password) {
+        return axios.get('http://localhost:8080/basicauth', 
+            {headers: {authorization: this.createBasicAuthToken(username,password)}})
     }
+
+    executeJwtAuthenticationService(username, password) {
+        return axios.post('http://localhost:8080/authenticate', {
+            username,
+            password
+        })
+    }
+
+    createBasicAuthToken(username,password) {
+        return 'Basic ' +  window.btoa(username + ":" + password)
+    }
+
+    registerSuccessfulLogin(username,password){
+        //let basicAuthHeader = 'Basic ' +  window.btoa(username + ":" + password)
+        //console.log('registerSuccessfulLogin')
+        sessionStorage.setItem('authenticatedUser', username)
+        this.setupAxiosInterceptors(this.createBasicAuthToken(username,password))
+    }
+
+    registerSuccessfulLoginForJwt(username,token) {
+        sessionStorage.setItem('authenticatedUser', username)
+        this.setupAxiosInterceptors(this.createJWTToken(token))
+    }
+
+    createJWTToken(token) {
+        return 'Bearer ' +  token
+    }
+
 
     logout() {
         sessionStorage.removeItem('authenticatedUser');
@@ -13,6 +43,24 @@ class AuthenticationService {
         let user = sessionStorage.getItem('authenticatedUser')
         if(user===null) return false
         return true
+    }
+
+    getLoggedInUserName() {
+        let user = sessionStorage.getItem('authenticatedUser')
+        if(user===null) return ''
+        return user
+    }
+
+    setupAxiosInterceptors(token) {
+
+        axios.interceptors.request.use(
+            (config) => {
+                if(this.isUserLoggedIn()) {
+                    config.headers.authorization = token
+                }
+                return config
+            }
+        )
     }
 }
 
